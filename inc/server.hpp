a8c65505b3ca33583d93abfb0b3186adbd7a25c0
agserran <agustinsscc01@gmail.com>
2025-07-09 20:54:01 +0200

    rename sutDown by shutDown

--- inc/server.hpp
+++ inc/server.hpp
@@ -1,32 +1,34 @@
 #pragma once
 #ifndef SERVER_HPP
-# define SERVER_HPP
+#define SERVER_HPP
 
- #include "tintin.hpp"
+#include "tintin.hpp"
 
 #define MAX_CLIENTS 3
 #define MAX_EVENTS 10
 
 class Tintin;
 
-class Server {
-	private:
-		int										port;
-		struct sockaddr_in		servAddr;
-		int										socket_fd;
-		std::set<int>					clients;
-		Tintin* tintin = new	Tintin();
-		int										lock_fd;
-	public:
-		Server();
-		Server(const Server &copy);
-		Server& operator=(const Server &other);
-		~Server();
-		void		runServer();
-		void		signalHandler();
-		void		sutDown(int socket_fd, int epoll_fd);
-		void		createFile();
-		void		unlinkFile();
+class Server
+{
+private:
+	int port;
+	struct sockaddr_in servAddr;
+	int socket_fd;
+	std::set<int> clients;
+	Tintin *tintin = new Tintin();
+	int lock_fd;
+
+public:
+	Server();
+	Server(const Server &copy);
+	Server &operator=(const Server &other);
+	~Server();
+	void runServer();
+	void signalHandler();
+	void shutDown(int socket_fd, int epoll_fd);
+	void createFile();
+	void unlinkFile();
 };
 
 #endif
